b191d5bff3bdc1a1bf61fb481610c0ced371107f
Heather Miller <heather.miller@epfl.ch>
2014-11-18 16:03:00 +0100

    Website footprint in place, with README too!

---
+++ _layouts/submission.md
@@ -0,0 +1,6 @@
+---
+layout: page
+title: Submission Information
+---
+
+Page about submission info.
---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+---
+---
+
+$(document).ready(function(){
+
+  // get current year and put it in span
+  var currYear = new Date().getFullYear()
+  $(".current-year").text(currYear);
+
+});
